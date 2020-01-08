e2c256e318555fe8e4df69f36eeb309d4f9dc264
HAL-42 <hal_42@zju.edu.cn>
2020-01-08 15:50:16 +0800

    #Create __init__.py for alchemy_cat

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Xiaobo Yang
+@contact: hal_42@zju.edu.cn
+@software: PyCharm
+@file: __init__.py
+@time: 2020/1/8 0:15
+@desc:
+"""
+from alchemy_cat.visualization.utils import CHW2HWC, HWC2CHW, RGB2BGR, BGR2RGB
+
+from alchemy_cat.py_tools import quick_init
