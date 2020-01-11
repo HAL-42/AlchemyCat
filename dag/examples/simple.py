93feb1ac1d851500d9491d8df3209397daef9bca
HAL-42 <hal_42@zju.edu.cn>
2020-01-12 05:27:15 +0800

    #Add parallel to simple.py

--- dag/examples/simple.py
+++ dag/examples/simple.py
@@ -2,22 +2,20 @@ import numpy as np
 
 from alchemy_cat.dag.core import Graph
 
+graph = Graph(pool_size=2, verbosity=2, slim=False)
+
+@graph.register()
+def f_my_function(a, b):
+    c = a + b
+    return c
+@graph.register(inputs=['d', 'a'], outputs=['e'])
+def f_my_function3(d, a):
+    e = d - a
+    return e
+@graph.register(outputs=['d'])
+def f_my_function2(c):
+    return c / 10.
 
 if __name__ == '__main__':
-    graph = Graph(verbosity=2, slim=False)
-
-    @graph.register()
-    def f_my_function(a, b):
-        c = a + b
-        return c
-    @graph.register(inputs=['d', 'a'], outputs=['e'])
-    def f_my_function3(d, a):
-        e = d - a
-        return e
-    @graph.register(outputs=['d'])
-    def f_my_function2(c):
-        return c / 10.
-
     res = graph.calculate(data={'a': np.random.randn(2000, 2000), 'b': np.random.randn(2000, 2000)})
-
     print(res)
