f6afd0d6f7aad9f4ad26412804c5a4b0f3e3e632
HAL-42 <hal_42@zju.edu.cn>
2020-01-15 15:40:10 +0800

    #Update init

--- py_tools/__init__.py
+++ py_tools/__init__.py
@@ -13,3 +13,4 @@ from alchemy_cat.py_tools.process_bar import ProcessBar
 from alchemy_cat.py_tools.quick_init import quick_init
 from alchemy_cat.py_tools.is_type import is_float, is_int, is_intarr, is_floatarr
 from alchemy_cat.py_tools.timer import Timer
+from alchemy_cat.py_tools.func_tools import Compose, Lambda
