a85dfcf500c2d41992806b810524d659e97a5123
HAL-42 <hal_42@zju.edu.cn>
2020-01-26 21:50:30 +0800

    #Create torch_tools' __init__.py

---
+++ torch_tools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Xiaobo Yang
+@contact: hal_42@zju.edu.cn
+@software: PyCharm
+@file: __init__.py
+@time: 2020/1/16 4:46
+@desc:
+"""
+from alchemy_cat.torch_tools.env import get_device, init_env, open_config
