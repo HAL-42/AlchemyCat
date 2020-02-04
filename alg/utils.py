f24af635cc2ee7a1726e7fc4e80d8ccabcffcb3c
Yang Xiaobo <hal_42@zju.edu.cn>
2020-02-04 11:51:47 +0800

    #Add color2scalar #Refine input check for size2HW

--- alg/utils.py
+++ alg/utils.py
@@ -10,12 +10,12 @@
 """
 from collections.abc import Iterator
 
-from typing import Any, Tuple, Iterable, Callable
+from typing import Any, Tuple, Iterable, Callable, Union
 
-from alchemy_cat.py_tools import is_int
+from alchemy_cat.py_tools import is_int, is_float
 
 
-__all__ = ['accumulate', 'size2HW']
+__all__ = ['accumulate', 'size2HW', 'color2scalar']
 
 # Taken from python 3.5 docs
 def accumulate(iterable: Iterable, fn: Callable=lambda x, y: x + y):
@@ -42,12 +42,32 @@ def size2HW(size: Any) -> Tuple[int, int]:
     Returns: (H, W) converted from size
     """
     if is_int(size):
+        if size < 0:
+            raise ValueError(f"size{size} must >= 0")
         return int(size), int(size)
     if isinstance(size, Iterator):
         return size2HW(list(size))
     elif len(size) == 2 and is_int(size[0]) and is_int(size[1]):
+        if size[0] < 0 or size[1] < 0:
+            raise ValueError(f"H, W of size {size} must >= 0")
         return int(size[0]), int(size[1])
     else:
         raise ValueError("size should be int or [int, int] or (int, int)")
 
 
+def color2scalar(val: Union[int, float, Iterable]) -> Tuple:
+    """Convert color value to color scalar
+
+    Args:
+        val (Union[int, float, Iterable]): If val is int or float, return (val, val, val), if value is Iterable with 3 element, return Tuple(val), else raise error
+
+    Returns: tuple color scalar
+    """
+    if is_int(val) or is_float(val):
+        img_pad_scalar = (val,) * 3
+    else:
+        img_pad_scalar = tuple(val)
+        if len(img_pad_scalar) != 3:
+            raise ValueError(f"If img_pad_value {val} is scalar, it should be a 3-channel color scalar")
+
+    return img_pad_scalar
