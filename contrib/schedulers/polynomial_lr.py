fa38d0b174fd610d0f5800485ec4cc0a4762daed
Yang Xiaobo <hal_42@zju.edu.cn>
2020-03-25 03:51:53 +0800

    #Refine param name #Add __repr__

--- contrib/schedulers/polynomial_lr.py
+++ contrib/schedulers/polynomial_lr.py
@@ -8,28 +8,36 @@
 @time: 2020/2/25 11:57
 @desc:
 """
+from typing import Any, Union
+
 import torch
 from torch.optim.lr_scheduler import _LRScheduler
 
-from typing import Union
+from alchemy_cat.py_tools import indent
 
 
 class PolynomialLR(_LRScheduler):
-    def __init__(self, optimizer: torch.optim.optimizer.Optimizer, step_size: int, iter_max: int,
+    def __init__(self, optimizer: Any, step_size: int, max_iter: int,
                  power: Union[int, float], last_epoch: int=-1):
         self.step_size = step_size
-        self.iter_max = iter_max
+        self.max_iter = max_iter
         self.power = power
         super(PolynomialLR, self).__init__(optimizer, last_epoch)
 
     def _polynomial_decay(self, lr):
-        return lr * (1 - float(self.last_epoch) / self.iter_max) ** self.power
+        return lr * (1 - float(self.last_epoch) / self.max_iter) ** self.power
 
     def get_lr(self):
         if (
             (self.last_epoch == 0)
             or (self.last_epoch % self.step_size != 0)
-            or (self.last_epoch > self.iter_max)
+            or (self.last_epoch > self.max_iter)
         ):
             return [group["lr"] for group in self.optimizer.param_groups]
         return [self._polynomial_decay(lr) for lr in self.base_lrs]
+
+    def __repr__(self):
+        return f"PolynomialLR <{self.__class__}>: \n" \
+               + indent(f"step_size: {self.step_size}") + '\n' \
+               + indent(f"max_iter: {self.max_iter}") + '\n' \
+               + indent(f"power: {self.power}")
