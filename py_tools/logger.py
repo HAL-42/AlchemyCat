dd2d8c86f3f07be4a413732dca660ca87d7e89f4
HAL-42 <hal_42@zju.edu.cn>
2021-03-02 20:40:09 +0800

    #Add silence option to Logger
    Output to terminal will be suppressed if Logger's silence=True

--- py_tools/logger.py
+++ py_tools/logger.py
@@ -15,22 +15,29 @@ import os.path as osp
 class Logger(object):
     """After call Logger(outfile), the stdout will print to both stdout and out_file"""
 
-    def __init__(self, out_file, real_time: bool=False):
+    def __init__(self, out_file, real_time: bool=False, silence: bool=False):
         """After call Logger(outfile), the stdout will print to both stdout and out_file
 
         Args:
             out_file: File where record the stdout
             real_time: If True, log file will flush after every write() call. (Default: False)
+            silence: If True, output to terminal will be suppressed. (Default: False)
         """
         self.terminal = sys.stdout
+        sys.stdout = self
+
         os.makedirs(osp.dirname(out_file), exist_ok=True)
         self.log = open(out_file, "w")
-        sys.stdout = self
+
         self.real_time = real_time
+        self.silence = silence
 
     def write(self, message):
-        self.terminal.write(message)
+        if not self.silence:
+            self.terminal.write(message)
+
         self.log.write(message)
+
         if self.real_time:
             self.log.flush()
 
