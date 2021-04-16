ab8e8aca9ef78be0804d4624d6ccc364b1897588
HAL-42 <hal_42@zju.edu.cn>
2021-04-16 22:45:52 +0800

    #Add clip_probs for numpy tensor in clamp_softmax

--- alg/clamp_softmax.py
+++ alg/clamp_softmax.py
@@ -8,10 +8,11 @@
 @time: 2020/3/28 23:02
 @desc:
 """
+import numpy as np
 import torch
 import torch.nn.functional as F
 
-__all__ = ['clamp_probs', 'clamp_softmax']
+__all__ = ['clamp_probs', 'clamp_softmax', 'clip_probs']
 
 
 def clamp_probs(probs: torch.Tensor, eps: float=1e-5) -> torch.Tensor:
@@ -41,3 +42,17 @@ def clamp_softmax(score_map: torch.Tensor, eps: float=1e-5):
         Clamped and normalized probs with shape (N, C, H, W)
     """
     return clamp_probs(F.softmax(score_map, 1), eps)
+
+def clip_probs(probs: np.ndarray, eps: float=1e-5) -> np.ndarray:
+    """Clip and normalize probs to make sure that min prob in probs won't be too small
+
+    Args:
+        probs: (N, C, H, W) probs
+        eps: Min prob allowed when clip probs
+
+    Returns:
+        Clip and normalized probs with shape (N, C, H, W)
+    """
+    clipped_probs = np.clip(probs, eps, 1)
+    normalized_probs = clipped_probs / np.sum(clipped_probs, axis=1, keepdims=True)
+    return normalized_probs
