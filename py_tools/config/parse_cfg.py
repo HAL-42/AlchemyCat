13d315baa4f1ec50396e4e9f5b9a357480b3a1c1
HAL-42 <hal_42@zju.edu.cn>
2023-12-19 16:53:20 +0800

    只是自定义parse时是否建立rslt_dir。

--- py_tools/config/parse_cfg.py
+++ py_tools/config/parse_cfg.py
@@ -8,11 +8,11 @@
 @Software: PyCharm
 @Desc    : 
 """
-from typing import Any
-
 import os
-from os import path as osp
 import warnings
+from os import path as osp
+from pathlib import Path
+from typing import Any
 
 from .open_cfg import open_config
 from .py_cfg import Config
@@ -31,33 +31,39 @@ def _check_emtpy_value(val: Any, memo: str='base.'):
         pass
 
 
-def auto_rslt_dir(file: str, config_root: str='./configs') -> str:
+def auto_rslt_dir(file: str, config_root: str='./configs', trunc_cwd: bool=False) -> str:
     """根据配置文件的__file__属性，自动返回配置文件的rslt_dir。
 
     Args:
         file: 配置文件的__file__属性。
         config_root: 相对工作目录，路径之于配置目录者。
+        trunc_cwd: 是否去掉当前工作目录。
 
     Returns:
         配置文件的rslt_dir，默认是__file__去掉配置目录和配置文件后的路径。
     """
-    return osp.dirname(osp.relpath(file, config_root))
+    if not trunc_cwd:
+        return osp.dirname(osp.relpath(file, config_root))
+    else:
+        return str(Path(file).relative_to(Path.cwd()).relative_to(config_root).parent)
 
 
-def _process_yaml_config(config: dict, experiments_root: str):
+def _process_yaml_config(config: dict, experiments_root: str, create_rslt_dir: bool=True):
     assert config.get('EXP_DIR') is None and config.get('TRAIN_DIR') is None and config.get('TEST_DIR') is None
     EXP_ID = config.get('EXP_ID')
     if EXP_ID is None:
         raise ValueError("CONFIG.EXP_ID must be defined")
     EXP_DIR = osp.join(experiments_root, str(EXP_ID))
-    os.makedirs(EXP_DIR, exist_ok=True)
+    if create_rslt_dir:
+        os.makedirs(EXP_DIR, exist_ok=True)
     config['EXP_DIR'] = EXP_DIR
     rslt_dir = EXP_DIR
 
     TRAIN_ID = config.get('TRAIN_ID')
     if TRAIN_ID is not None:
         TRAIN_DIR = osp.join(EXP_DIR, 'trains', str(TRAIN_ID))
-        os.makedirs(TRAIN_DIR, exist_ok=True)
+        if create_rslt_dir:
+            os.makedirs(TRAIN_DIR, exist_ok=True)
         rslt_dir = TRAIN_DIR
     else:
         TRAIN_DIR = EXP_DIR
@@ -69,7 +75,8 @@ def _process_yaml_config(config: dict, experiments_root: str):
             TEST_DIR = osp.join(TRAIN_DIR, 'tests', str(TEST_ID))
         else:
             TEST_DIR = osp.join(EXP_DIR, 'tests', str(TEST_ID))
-        os.makedirs(TEST_DIR, exist_ok=True)
+        if create_rslt_dir:
+            os.makedirs(TEST_DIR, exist_ok=True)
         rslt_dir = TEST_DIR
     else:
         TEST_DIR = EXP_DIR
@@ -80,7 +87,8 @@ def _process_yaml_config(config: dict, experiments_root: str):
     return config
 
 
-def _process_py_config(config: dict, config_path: str, experiments_root: str, config_root: str):
+def _process_py_config(config: dict, config_path: str, experiments_root: str, config_root: str,
+                       create_rslt_dir: bool=True):
     if isinstance(config, Config):
         # TODO 支持下级cfg删除上级cfg项。
         config.update_at_parser()
@@ -97,12 +105,14 @@ def _process_py_config(config: dict, config_path: str, experiments_root: str, co
         config['rslt_dir'] = auto_rslt_dir(config_path, config_root)
 
     config['rslt_dir'] = osp.join(experiments_root, config['rslt_dir'])
-    os.makedirs(config['rslt_dir'], exist_ok=True)
+    if create_rslt_dir:
+        os.makedirs(config['rslt_dir'], exist_ok=True)
 
     return config
 
 
-def parse_config(config_path: str, experiments_root: str, config_root: str='./configs') -> Config:
+def parse_config(config_path: str, experiments_root: str=None, config_root: str='./configs',
+                 create_rslt_dir: bool=True) -> Config:
     """Parse config from config path.
 
     This function will read yaml config from config path then create experiments dirs according to config file.
@@ -111,10 +121,17 @@ def parse_config(config_path: str, experiments_root: str, config_root: str='./co
         config_path: Path of yaml config.
         experiments_root: Root dictionary for experiments.
         config_root: Root dictionary of configs, for auto_rslt_dir only.
+        create_rslt_dir: Whether to create rslt_dir.
 
     Returns:
         YAML config in Addict format
     """
+    if create_rslt_dir and experiments_root is None:
+        # NOTE 如果需要创建rslt_dir，那么experiments_root必须提供。
+        # NOTE 相反，若之后experiments_root为None，肯定不需要创建rslt_dir，此时experiment_root只用于修正原来的rslt_dir。
+        # NOTE 因此，不妨在这种情况下，将experiments_root视作''。
+        raise ValueError("experiments_root must be provided when create_rslt_dir is True")
+
     # * Open config, get config dict.
     config, is_py = open_config(config_path, is_yaml=False)
     if config is None:
@@ -122,9 +139,11 @@ def parse_config(config_path: str, experiments_root: str, config_root: str='./co
 
     # * Create experiment dirs according to CONFIG
     if not is_py:
-        config = _process_yaml_config(config, experiments_root)
+        config = _process_yaml_config(config, '' if experiments_root is None else experiments_root, create_rslt_dir)
     else:
-        config = _process_py_config(config, config_path, experiments_root, config_root=config_root)
+        config = _process_py_config(config, config_path, '' if experiments_root is None else experiments_root,
+                                    config_root=config_root,
+                                    create_rslt_dir=create_rslt_dir)
 
     # * Check empty value and return
     _check_emtpy_value(config, memo='config.')
