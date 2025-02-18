6c75dc51f984974d3faff2e0c3be923894f0ff99
HAL-42 <hal_42@zju.edu.cn>
2025-02-18 21:24:14 +0800

    增加dt_constant模块，其记载了深度学习中的常用参数

---
+++ alchemy_cat/contrib/constant/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2025/2/9 17:53
+@File    : __init__.py.py
+@Software: PyCharm
+@Desc    : 
+"""
---
+++ alchemy_cat/contrib/constant/dt_constant.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : Xiaobo Yang
+@Contact : hal_42@zju.edu.cn
+@Time    : 2025/2/9 17:54
+@File    : dt_constant.py
+@Software: PyCharm
+@Desc    : 
+"""
+import typing as t
+
+type_std_mean: t.TypeAlias = tuple[float, float, float]
+
+OPENAI_DATASET_MEAN: t.Final[type_std_mean] = (0.48145466, 0.4578275, 0.40821073)
+OPENAI_DATASET_STD: t.Final[type_std_mean] = (0.26862954, 0.26130258, 0.27577711)
+IMAGENET_MEAN: t.Final[type_std_mean] = (0.485, 0.456, 0.406)
+IMAGENET_STD: t.Final[type_std_mean] = (0.229, 0.224, 0.225)
+INCEPTION_MEAN: t.Final[type_std_mean] = (0.5, 0.5, 0.5)
+INCEPTION_STD: t.Final[type_std_mean] = (0.5, 0.5, 0.5)
