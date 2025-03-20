3c345e05ae4ba16fc4d3c9bce210822c0ff8eda3
HAL-42 <hal_42@zju.edu.cn>
2025-03-20 20:38:47 +0800

    添加_stream_logger的reset方法以支持流的重置和属性转发

--- alchemy_cat/py_tools/logger.py
+++ alchemy_cat/py_tools/logger.py
@@ -29,8 +29,11 @@ class _StreamLogger(object):
 
         self.log = log
 
+        self.stream_name = stream_name
         self.real_time = real_time
         self.silence = silence
+
+        self._is_reset = False
 
     def write(self, message):
         if not self.silence:
@@ -45,6 +48,31 @@ class _StreamLogger(object):
         self.stream.flush()
         self.log.flush()
 
+    def reset(self):
+        if self._is_reset:
+            return
+
+        if self.stream_name == 'stdout':
+            sys.stdout = self.stream
+        elif self.stream_name == 'stderr':
+            sys.stderr = self.stream
+        else:
+            raise ValueError(f"stream_name = {self.stream_name} should be 'stdout' or 'stderr'.")
+        self.stream = None
+        self.log = None
+
+        self._is_reset = True
+
+    def __getattr__(self, name):
+        """Forward attribute access to the underlying stream for undefined attributes."""
+        if self._is_reset:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}' (object has been reset)")
+        if self.stream is None:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}' (stream is None)")
+
+        # Let getattr raise AttributeError directly if attribute doesn't exist on stream
+        return getattr(self.stream, name)
+
 
 class Logger(object):
     """After call Logger(outfile), the stdout will print to both stdout and out_file"""
@@ -57,6 +85,8 @@ class Logger(object):
             real_time: If True, log file will flush after every write() call. (Default: False)
             silence: If True, output to terminal will be suppressed. (Default: False)
         """
+        if isinstance((old_logger := getattr(sys, "alchemy_cat_logger", None)), Logger):
+            old_logger.reset()
         sys.alchemy_cat_logger = self  # Make Logger won't be deleted.
 
         os.makedirs(osp.dirname(out_file), exist_ok=True)
@@ -68,7 +98,24 @@ class Logger(object):
         self.stdout_logger = _StreamLogger('stdout', self.log, self.real_time, self.silence)
         self.stderr_logger = _StreamLogger('stderr', self.log, self.real_time, self.silence)
 
-    def __del__(self):
+        self._is_reset = False
+
+    def reset(self):
+        if self._is_reset:
+            return
+
         self.stdout_logger.flush()
         self.stderr_logger.flush()
+
+        self.stdout_logger.reset()
+        self.stderr_logger.reset()
+
         self.log.close()
+
+        if getattr(sys, "alchemy_cat_logger", None) is self:
+            del sys.alchemy_cat_logger
+
+        self._is_reset = True
+
+    def __del__(self):
+        self.reset()
